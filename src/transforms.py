f9555375baa0c0a5a213d9f139e112500e1d3b60
Joergenator <joergentro@hotmail.com>
2026-04-23 10:52:09 +0200

    merge training infrastructure from v4 branch

--- src/transforms.py
+++ src/transforms.py
@@ -7,7 +7,17 @@ IMAGENET_STD = [0.229, 0.224, 0.225]
 IMG_SIZE = 224
 
 
-def get_train_transforms():
+def get_train_transforms(augment_level="standard"):
+    if augment_level == "mild":
+        return transforms.Compose([
+            transforms.Resize((IMG_SIZE, IMG_SIZE)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomResizedCrop(IMG_SIZE, scale=(0.8, 1.0)),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.1),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
+            transforms.RandomErasing(p=0.1),
+        ])
     return transforms.Compose([
         transforms.Resize((IMG_SIZE, IMG_SIZE)),
         transforms.RandomHorizontalFlip(),
